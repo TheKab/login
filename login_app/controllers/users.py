10bf30e8f3e5966759531d8b8dfa801bc43f8f05
Kab <sye.roble@gmail.com>
2022-06-22 20:40:22 -0400

    master

--- login_app/controllers/users.py
+++ login_app/controllers/users.py
@@ -21,7 +21,7 @@ def display_login():
 def user_login():
     #Use validate_login staticmethod to validate login form
     if User.validate_login(request.form) == False:
-        return redirect("/login")
+        return redirect("/")
     #Else get user from DB
     else:
         result = User.get_one(request.form)
@@ -29,11 +29,10 @@ def user_login():
         #If not in DB 
         if result == None:
             flash("Wrong credentials", "error_login")
-            return redirect("/login")
+            return redirect("/")
         #Else through session pass credentials to open homepage
         else:
             session['email']    = result.email
-            session['password'] = result.password
             session['id']       = result.id
             session['first_name'] = result.first_name
 
@@ -45,31 +44,30 @@ def user_login():
 @app.route('/homepage')
 def homepage():
     if User.validate_session() == False:
-        return redirect("/login")
+        return redirect("/")
     else:
-        name = session['first_name']
-        return render_template("homepage.html", first_name = name)
+        data = {
+            'email' : session['email']
+        }
+        one_user = User.get_one(data)
+        return render_template("homepage.html", one_user = one_user)
 
 
 # -----------------  REGISTER NEW USER WITH VALIDATION -------------------------
-
-@app.route("/users/new")
-def new_user_form():
-    return render_template("register_user.html")
-
 
 @app.route("/users/register_user", methods=["POST"])
 def register_user():
     #Use validate_login staticmethod to validate register form = flash messages
     if User.validate_user(request.form) == False:
-        return redirect("/login")
+        return redirect("/")
     #Else get user from DB
     else:
         result = User.get_one(request.form)
+
         #If not in DB 
-        if result == None:
-            flash("Wrong credentials", "error_login")
-            return redirect("/login")
+        if result is not None:
+            flash("Email is taken.", "error_login")
+            return redirect("/")
         #Else through pass credentials to create user
         else:
             data = {
@@ -78,11 +76,13 @@ def register_user():
                 "email"         : request.form["email"],
                 "password"      : bcrypt.generate_password_hash(request.form['password'])
             }
+            new_user_id = User.save(data)
 
-        User.save(data)
+            #create session to pass through to homepage - New Registered User is logged in.
+            session['email']    = data['email']
+            session['id']       = new_user_id
 
-        return redirect('/')
-
+        return redirect('/homepage')
 
 
 # -----------------  LOGOUT -------------------------
